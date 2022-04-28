f41c353442bfdc987baab31d596ca8358fad429d
Elif Kaya <84388617+elifkaya3@users.noreply.github.com>
2022-04-28 14:51:51 +0300

    28.04

---
+++ Week13/UploadImageProject/UploadImageProject/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UploadImageProject.Models;
+
+namespace UploadImageProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult UploadImage()
+        {
+            //path dosya yolu
+            string path = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot/images/");
+            //Birazdan resmin yüklenmesini işlemini tamamlayıp döneceğiz
+            var imageList = Directory.GetFiles(path);
+
+            List<UploadImageModel> uploadImages = new List<UploadImageModel>();
+
+            foreach (var image in imageList)
+            {
+                FileInfo fileInfo = new FileInfo(image);
+                UploadImageModel model = new UploadImageModel();
+                model.FullName = image.Substring(image.IndexOf("wwwroot")).Replace("wwwroot/", string.Empty);
+                model.FileName = fileInfo.Name;
+                model.Size = fileInfo.Length / 1024;
+                uploadImages.Add(model);
+            }
+
+            return View(uploadImages);
+        }
+        [HttpPost]
+        public IActionResult UploadImage(IFormFile file)
+        {
+            if (file != null)
+            {
+                string imageExtension = Path.GetExtension(file.FileName);
+                string imageName = Guid.NewGuid() + imageExtension;
+                string path = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot/images/{imageName}");
+                var stream = new FileStream(path, FileMode.Create);
+                file.CopyTo(stream);
+            }
+
+            return RedirectToAction("UploadImage");
+        }
+       
+    }
+}
