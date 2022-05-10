f9e7babb70dd902a11b1f8f527d9c4e4a80a7f03
Elif Kaya <84388617+elifkaya3@users.noreply.github.com>
2022-05-10 15:17:32 +0300

    10/05

--- Week15/YemekTarifleriApp/YemekTarifleriApp.Entity/Member.cs
+++ Week15/YemekTarifleriApp/YemekTarifleriApp.Entity/Member.cs
@@ -10,7 +10,7 @@ namespace YemekTarifleriApp.Entity
     {
         public int MemberId { get; set; }
         public string MemberName { get; set; }
-        public string EMail { get; set; }
+        public string MemberMail { get; set; }
         public string MemberUserName { get; set; }
         public char MemberPassword { get; set; }
         public List<Recipe> Recipes { get; set; }
