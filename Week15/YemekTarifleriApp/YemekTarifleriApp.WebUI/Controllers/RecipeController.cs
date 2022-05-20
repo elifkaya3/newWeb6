4c7dd6686a9ada4a1db11ff6dfe23c97b7a19cd2
Elif Kaya <84388617+elifkaya3@users.noreply.github.com>
2022-05-20 14:15:55 +0300

    yemek tarifleri update

---
+++ Week15/YemekTarifleriApp/YemekTarifleriApp.WebUI/Controllers/RecipeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YemekTarifleriApp.Bussiness.Abstract;
+
+namespace YemekTarifleriApp.WebUI.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private IRecipeService _recipeService;
+        public RecipeController(IRecipeService recipeService)
+        {
+            _recipeService = recipeService;
+        }
+        public IActionResult Index()
+        {
+            return View(_recipeService.GetAll());
+        }
+        //public IActionResult RecipeDetails()
+        //{
+        //    return View(_recipeService.GetAll());
+        //}
+    }
+}
