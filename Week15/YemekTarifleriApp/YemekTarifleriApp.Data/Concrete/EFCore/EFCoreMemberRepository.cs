eb43af77f80e1df6f30e4aa00f5de49feb5d7343
Elif Kaya <84388617+elifkaya3@users.noreply.github.com>
2022-05-25 14:06:49 +0300

    25/05

--- Week15/YemekTarifleriApp/YemekTarifleriApp.Data/Concrete/EFCore/EFCoreMemberRepository.cs
+++ Week15/YemekTarifleriApp/YemekTarifleriApp.Data/Concrete/EFCore/EFCoreMemberRepository.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YemekTarifleriApp.Data.Abstract;
 using YemekTarifleriApp.Entity;
 
 namespace YemekTarifleriApp.Data.Concrete.EFCore
 {
-    public class EFCoreMemberRepository:EFCoreGenericRepository<Member,RecipeAppContext>
+    public class EFCoreMemberRepository:EFCoreGenericRepository<Member,RecipeAppContext>,IMemberRepository
     {
         //ekstra metod varsa implement edilecek
     }
