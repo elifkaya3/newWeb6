be8b0d67f48fb9d288bd2a2fbb861b9cb15809f3
Elif Kaya <84388617+elifkaya3@users.noreply.github.com>
2022-06-07 15:00:59 +0300

    7 haziran

---
+++ Week14/MiniShopApp/MiniShopApp.WebUI/Controllers/CardController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MiniShopApp.Business.Abstract;
+using MiniShopApp.WebUI.Identity;
+using MiniShopApp.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniShopApp.WebUI.Controllers
+{
+    [Authorize]
+    public class CardController : Controller
+    {
+        private ICardService _cardService;
+        private UserManager<User> _userManager;
+
+        public CardController(ICardService cardService, UserManager<User> userManager)
+        {
+            _cardService = cardService;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var card = _cardService.GetCardByUserId(_userManager.GetUserId(User));
+            var model = new CardModel()
+            {
+                CardId = card.Id,
+                CardItems = card.CardItems.Select(i => new CardItemModel()
+                {
+                    CardItemId=i.Id,
+                    ProductId=i.ProductId,
+                    Name=i.Product.Name,
+                    Price=(double)i.Product.Price,
+                    ImageUrl=i.Product.ImageUrl,
+                    Quantity=i.Quantity
+                }).ToList()
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult AddToCard(int productId,int quantity)
+        {
+            //user bilgisinden yararlanarak userID bulalım.
+            //userId,productId,quantity bilgilerini kullanıp bize carda ürün 
+            //ekleme işlemini yapıp dönecek bir metot hazırlayalım
+
+            var userId = _userManager.GetUserId(User);
+            _cardService.AddToCard(userId, productId, quantity);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult DeleteFromCard(int productId)
+        {
+            var userId = _userManager.GetUserId(User);
+            _cardService.DeleteFromCard(userId, productId);
+            return RedirectToAction("Index");
+        }
+        public IActionResult CheckOut()
+        {
+            return View();
+        }
+    }
+}
